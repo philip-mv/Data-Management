7e5445e84b7baf869f08aa099a6326c8eb4b8232
philip-mv <philipmv10@gmail.com>
2025-06-04 16:27:50 +0530

    aknckua

---
+++ Lesson - 7/NUmberofbittu/sbnn.py
@@ -0,0 +1,8 @@
+def numberofbittu(n):
+    count = 0
+    while (n):
+        count+= 1
+        n >>= 1
+    return count
+n = int(input("Enter a number :"))
+print("Number of bits :",numberofbittu(n))
