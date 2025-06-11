329b0e9d3c9dd2138491af20f5b0b20d165132f4
philip-mv <philipmv10@gmail.com>
2025-06-11 15:25:09 +0530

    scn

---
+++ Lesson - 7/Implement Circuit/nskfbckejn.py
@@ -0,0 +1,17 @@
+def solve_circuit(A, B, C):
+    
+    return B
+
+
+A_example = True
+B_example = False
+C_example = True
+output_example = solve_circuit(A_example, B_example, C_example)
+print(f"For A={A_example}, B={B_example}, C={C_example}, the output Q is: {output_example}")
+
+
+A_example2 = False
+B_example2 = True
+C_example2 = False
+output_example2 = solve_circuit(A_example2, B_example2, C_example2)
+print(f"For A={A_example2}, B={B_example2}, C={C_example2}, the output Q is: {output_example2}")
