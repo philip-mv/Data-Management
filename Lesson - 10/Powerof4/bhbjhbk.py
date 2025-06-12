6570798946a05eb704fda02eba6e6323b2d9e118
philip-mv <philipmv10@gmail.com>
2025-06-12 16:21:01 +0530

    jkjkjkjkjk

---
+++ Lesson - 10/Powerof4/bhbjhbk.py
@@ -0,0 +1,28 @@
+
+def powerof4(number):
+    count = 0
+   
+    if number <= 0:
+        return False
+
+    
+    if (number & (number - 1)) != 0: 
+        return False
+    
+    while(number > 1):
+        number >>= 1
+        count += 1
+    
+   
+    if(count % 2 == 0):
+        return True
+    else:
+        return False
+
+number = int(input("Enter your number : "))
+
+
+if(powerof4(number)):
+    print(number,'is a power of 4') 
+else:
+    print(number,'is not a power of 4') 
