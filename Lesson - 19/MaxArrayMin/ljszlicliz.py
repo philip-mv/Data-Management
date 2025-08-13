22bb9065b9f0b97de3b47fa5f30b72033c6fb3b7
philip-mv <philipmv10@gmail.com>
2025-08-13 16:28:49 +0530

    gjldzfLKShdLSLhL

---
+++ Lesson - 19/MaxArrayMin/ljszlicliz.py
@@ -0,0 +1,14 @@
+def minele(a,size):
+    temp = a[0]
+    for i in range(1,size):
+        temp = min(temp,a[i])
+    return temp
+def maxele(a,size):
+    temp = a[0]
+    for i in range(1,size):
+        temp = max(temp,a[i])
+    return temp
+a = [12,1234,45,67,1]
+size = len(a)
+print('Minimum element of array :',minele(a,size))
+print('Maximum element of array :',maxele(a,size))
