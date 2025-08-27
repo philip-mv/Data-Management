6a4aea689e166c45e2db1a714a86c353de3985b0
philip-mv <philipmv10@gmail.com>
2025-08-27 16:08:01 +0530

    wawakxnl

---
+++ Lesson - 22/Maximum1s/jaoufdoawifswfa.py
@@ -0,0 +1,13 @@
+def getMaxlength(a,a_size):
+    counter = 0
+    maxOnes = 0
+    for i in range(0,a_size):
+        if(a[i] == 0):
+            counter = 0
+        else :
+            counter += 1
+            maxOnes = max(maxOnes,counter)
+    return maxOnes
+a = [1,1,0,0,0,0,1,1,1,1]
+a_size = len(a)
+print("Max 1's :",getMaxlength(a,a_size))
