8c173b2bf7aa3d7c95b43d4971b30ab05053b655
philip-mv <philipmv10@gmail.com>
2025-09-03 16:08:10 +0530

    ewijiwf

---
+++ Lesson - 24/EquilibriumPOint/pointu.py
@@ -0,0 +1,16 @@
+def equilibriumPoint(arr):
+    leftSideSum = 0
+    rightSideSum = 0
+    n = len(arr)
+    for i in range(n):
+        leftSideSum = 0
+        rightSideSum = 0
+        for j in range(n):
+            leftSideSum += arr[j]
+        for j in range(i + 1,n):
+            rightSideSum += arr[j]
+        if leftSideSum == rightSideSum:
+            return i
+    return - 1
+arr = [-4,6,2,0,0,1,1]
+print("Element :",arr[equilibriumPoint(arr)])
